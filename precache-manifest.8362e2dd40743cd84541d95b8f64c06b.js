9126bc9cefb43817ec307fbaea9c72bd07c19bdc
13816576524 <13816576524@sina.cn>
2020-08-06 08:24:42 +0800

    1.0

---
+++ precache-manifest.8362e2dd40743cd84541d95b8f64c06b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9782abdb50656f898e3e935695016d9d",
+    "url": "/MUIApp/index.html"
+  },
+  {
+    "revision": "a4ebf52f185b472f2116",
+    "url": "/MUIApp/static/css/2.33bbdc9a.chunk.css"
+  },
+  {
+    "revision": "bd72b372688ffd5a26be",
+    "url": "/MUIApp/static/css/main.a93b153c.chunk.css"
+  },
+  {
+    "revision": "a4ebf52f185b472f2116",
+    "url": "/MUIApp/static/js/2.608adf25.chunk.js"
+  },
+  {
+    "revision": "bd72b372688ffd5a26be",
+    "url": "/MUIApp/static/js/main.a15789ad.chunk.js"
+  },
+  {
+    "revision": "8afc0bad1338707590d1",
+    "url": "/MUIApp/static/js/runtime~main.ad1a8192.js"
+  },
+  {
+    "revision": "4b576fdb387b0453a82e83c82a06607e",
+    "url": "/MUIApp/static/media/Carousel0.4b576fdb.jpg"
+  },
+  {
+    "revision": "2fcf7afc5853b3ba7ed0c2fa77146514",
+    "url": "/MUIApp/static/media/Carousel1.2fcf7afc.jpg"
+  },
+  {
+    "revision": "bce0b63056e6441c13116e09f4b3f91c",
+    "url": "/MUIApp/static/media/Carousel2.bce0b630.jpg"
+  },
+  {
+    "revision": "ca512eece584bb242cf4d210493ea1f9",
+    "url": "/MUIApp/static/media/Carousel3.ca512eec.jpg"
+  },
+  {
+    "revision": "6572f8d87c86bb932096962bfc2c469d",
+    "url": "/MUIApp/static/media/Carousel5.6572f8d8.jpg"
+  },
+  {
+    "revision": "718cd1d8149ec0b041d2e5b75cb89081",
+    "url": "/MUIApp/static/media/Carousel6.718cd1d8.jpg"
+  },
+  {
+    "revision": "367f1606093511a18a6a962ebba1b6fa",
+    "url": "/MUIApp/static/media/Carousel7.367f1606.jpg"
+  },
+  {
+    "revision": "379b90371773c6fda05b0357a33a119a",
+    "url": "/MUIApp/static/media/STKAITI.379b9037.TTF"
+  },
+  {
+    "revision": "1e973cdc0a1dfd7ccf8b481a4f0ff866",
+    "url": "/MUIApp/static/media/STZHONGS.1e973cdc.TTF"
+  },
+  {
+    "revision": "ce7751862b364e203241f059f439f32e",
+    "url": "/MUIApp/static/media/VippNeue-Regular.ce775186.eot"
+  },
+  {
+    "revision": "3e3bd9954630f673fbcb46bb72a46f21",
+    "url": "/MUIApp/static/media/WaterPump.3e3bd995.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MUIApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9e4d070009c01a7d5bb34ad9be621627",
+    "url": "/MUIApp/static/media/zeekong.9e4d0700.jpg"
+  }
+]);
